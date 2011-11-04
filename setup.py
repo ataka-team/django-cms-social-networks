e379c50445dff9895b3ac00f8118bd3cca17bbe9
Pablo Saavedra <saavedra.pablo@gmail.com>
2011-11-04 18:54:45 +0100

    - Fixed non utf8 characters on icense file. - setup.py file extended.

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     author = 'Pablo Saavedra',
     author_email = 'pablo.saavedra@treitos.com',
     url = 'http://github.com/psaavedra/django-cms-social-networks',
+    download_url= 'https://github.com/psaavedra/django-cms-social-networks/zipball/master',
     packages = find_packages(),
     package_data={
         'cms_pictures_polaroid': [
@@ -21,5 +22,17 @@ setup(
     zip_safe=False,
     install_requires=[
         "django-cms>=2.1",
-    ]
+    ],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Framework :: Django",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    long_description=file('README').read(),
+    license=file('MIT_License.txt').read(),
+    keywords = "social networks facebook comments",
 )
